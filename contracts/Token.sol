62bb3be8eb3bf16bc1de7c6dc64c190ec8fab399
Claudio Hermida <13628966+claudiohermida@users.noreply.github.com>
2025-06-21 16:10:40 -0300

    Create Token.sol

---
+++ contracts/Token.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.27;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    cconstructor(string memory name, string memory symbol)
+        ERC20(name, symbol)
+        Ownable(msg.sender)
+    {}
+}
