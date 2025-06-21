670126a2e6abacd74b31ea40ad912e0fa9881c07
Claudio Hermida <13628966+claudiohermida@users.noreply.github.com>
2025-06-21 16:13:51 -0300

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,7 +6,7 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
-    cconstructor(string memory name, string memory symbol)
+    constructor(string memory name, string memory symbol)
         ERC20(name, symbol)
         Ownable(msg.sender)
     {}
